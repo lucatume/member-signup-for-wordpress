fa10cc477c73f0e644af0b3a2899c3816b165b27
Luca Tumedei <luca@theaveragedev.com>
2013-10-23 10:40:45 +0200

    First compilation and magnification of the plugin styles and scripts. They are empty though.

---
+++ assets/js/admin.js
@@ -0,0 +1,13 @@
+/*! Member Signup - v0.1.0
+ * https://github.com/lucatume/member-signup-for-wordpress.git
+ * Copyright (c) 2013; * Licensed GPLv2+ */
+(function ( $ ) {
+	"use strict";
+
+	$(function () {
+
+		// Place your administration-specific JavaScript here
+
+	});
+
+}(jQuery));
