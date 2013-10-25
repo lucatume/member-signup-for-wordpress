111818c2f8abc496a1cc566588b3f5353b211032
Luca Tumedei <luca@theaveragedev.com>
2013-10-25 09:41:20 +0200

    Clarified some comments

--- member_signup.php
+++ member_signup.php
@@ -42,7 +42,7 @@ require_once( MEMBERSIGNUP_PLUGIN_DIRPATH . 'includes/class-member_signup.php' )
 require_once( MEMBERSIGNUP_PLUGIN_DIRPATH . 'includes/class-member_signup-admin.php' );
 
 /**
- * Require more adapter classes
+ * Require more adapter classes used by both front-end and back-end classes
  */
 require_once ( MEMBERSIGNUP_PLUGIN_DIRPATH . 'includes/class-user_role_checker.php' );
 require_once ( MEMBERSIGNUP_PLUGIN_DIRPATH . 'includes/class-options_getter.php' );
@@ -55,7 +55,7 @@ register_activation_hook( __FILE__, array( 'membersignup', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'membersignup', 'deactivate' ) );
 
 /*
- * At init load our main plugin
+ * At init load our main plugin classes
  */
 add_action( 'init', array( 'membersignup', 'get_instance' ) );
 add_action( 'init', array( 'membersignup_Admin', 'get_instance' ) );
