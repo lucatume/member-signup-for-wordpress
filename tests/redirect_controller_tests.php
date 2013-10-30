5871c7ce9e7a5fa80c71444a334bffb5a174ba97
Luca Tumedei <luca@theaveragedev.com>
2013-10-30 20:14:31 +0100

    Created a first version of the Redirect Controller class

--- tests/redirect_controller_tests.php
+++ tests/redirect_controller_tests.php
@@ -4,6 +4,7 @@ class redirectControllerTest extends PHPUnit_Framework_TestCase
 	public function setUp()
 	{
 		// do something before each test
+		
 	}
 	public function test_invokes_add_action_only_once()
 	{
@@ -17,6 +18,24 @@ class redirectControllerTest extends PHPUnit_Framework_TestCase
 		$b = membersignup_Redirect_Controller::get_instance();
 		$this->assertTrue($mock_filter === $a->filters, 'should be using injected filter');
 		$this->assertTrue($mock_filter === $b->filters, 'should be using injected filter');
+	}
+	public function test_redirect_returns_for_logged_users()
+	{
+		// mock Functions
+		$mock_functions = $this->getMock('adclasses_Functions');
+		// mock Globals
+		$mock_globals = $this->getMock('adclasses_Globals');
+		// set is_user_logged_in
+		$mock_functions->expects($this->once())->method('is_user_logged_in')->will($this->returnValue(true));
+		// set pagenow not to be called
+		$mock_globals->expects($this->never())->method('pagenow');
+		// get an instance and inject mocks
+		$i = membersignup_Redirect_Controller::get_instance(array(
+			'functions' => $mock_functions,
+			'globals' => $mock_globals
+		));
+		// run the method
+		$i->redirect_to_member_login();
 	}
 	public function tearDown()
 	{
