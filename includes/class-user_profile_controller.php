bde7eb6d135e6b8484d28900c368907e28de519e
Luca Tumedei <luca@theaveragedev.com>
2013-11-08 21:31:02 +0100

    First version of the profile customisation class

--- includes/class-user_profile_controller.php
+++ includes/class-user_profile_controller.php
@@ -27,14 +27,14 @@ class membersignup_User_Profile_Controller implements adclasses_Singleton
 		if (!is_array($adapters)) {
 			// instance the adapters by itself
 			$adapters = adclasses_Factory::get_instance()->get_adapters(array(
-				'functions'
+				'functions',
+				'globals'
 			));
 		}
 		
 		foreach ($adapters as $key => $value) {
 			$this->{$key} = $value;
 		}
-		// hook into admin head and footer
 		$this->functions->add_action('admin_head', array(
 			$this,
 			'start_profile_buffer'
@@ -50,11 +50,57 @@ class membersignup_User_Profile_Controller implements adclasses_Singleton
 			self::$instance = null;
 		}
 	}
-	public function admin_head()
+	public function start_profile_buffer()
 	{
+		if ($this->should_modify_profile_fields()) {
+			// start buffering
+			ob_start();
+		}
 	}
-	public function admin_footer()
+	public function end_profile_buffer()
 	{
+		if ($this->should_modify_profile_fields()) {
+			$contents = ob_get_contents();
+			ob_end_clean();
+			// TODO: all the fields below should be options
+			// TODO: please make stronger regexes
+			// change the personal profile title
+			$contents = preg_replace("~(<div\\s*id\\s*=\\s*[\"']icon-profile.*<h2>)([^<]*)(</h2>)~uUs", "$1Dati Iscrizione$3", $contents);
+			//remove the personal informations title
+			$contents = preg_replace("~(<\\s*form\\s*id\\s*=\\s*['\"]your-profile.*)(<h3>.*)(<h3)~uUs", "$1$3", $contents, 1);
+			// change the username label
+			$contents = preg_replace("~(<label\\s*for\\s*=\\s*['\"]user_login['\"].*>)([^<]*?)(<.*<span class=\"description\">)([^<]*?)~uUs", "$1Codice Fiscale$3Il codice fiscale non puo' essere modificato", $contents);
+			// remove the nickname row
+			// remove the public name row
+			$contents = preg_replace("~<tr>\\s*<th>\\s*<label\\s*for\\s*=['\"]nickname['\"].*</tr>.*display_name.*/tr>~uUs", "", $contents);
+			// remove the website row
+			$contents = preg_replace("~<tr>\\s*<th>\\s*<label\\s*for\\s*=[\"']url['\"].*</tr>~uUs", "", $contents);
+			// remove the bio section
+			$contents = preg_replace("~<tr>\\s*<th>\\s*<label\\s*for\\s*=[\"']description['\"].*</tr>~uUs", "", $contents);
+			// remove all the titles
+			$contents = preg_replace("~(<\\s*h3\\s*>[^<]*<\\s*/h3\\s*>)\\s*(<\\s*table\\s*class\\s*=\\s*['\"]form-table)~uUs", "$2", $contents);
+			echo $contents;
+		}
+	}
+	public function should_modify_profile_fields()
+	{
+		// TODO: do some REAL check here
+		if ($this->functions->is_admin()) {
+			// TODO: should be an option
+			$roles = array(
+				'member',
+				'subscriber'
+			);
+			
+			foreach ($roles as $role) {
+				if (membersignup_User_Role_Checker::is_user_of_role($role)) {
+					
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
 ?>
