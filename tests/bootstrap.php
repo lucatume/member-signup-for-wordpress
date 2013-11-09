120bbb0fb06184308462c98ee7468983c8ee32f5
Luca Tumedei <luca@theaveragedev.com>
2013-11-09 09:05:04 +0100

    Replaced occurrences of __DIR__ with dirname

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,19 @@
 <?php
 // Include the interfaces
 
-foreach (glob(__DIR__ . '/../../adapter-classes-for-wordpress/includes/interface-*.php') as $filename) {
+foreach (glob(dirname(__FILE__) . '/../../adapter-classes-for-wordpress/includes/interface-*.php') as $filename) {
 	require_once $filename;
 }
 // Include the adapter classes
 
-foreach (glob(__DIR__ . '/../../adapter-classes-for-wordpress/includes/class-*.php') as $filename) {
+foreach (glob(dirname(__FILE__) . '/../../adapter-classes-for-wordpress/includes/class-*.php') as $filename) {
 	require_once $filename;
 }
 // Include all the plugin classes
 
-foreach (glob(__DIR__ . '/../includes/class-*.php') as $file) {
+foreach (glob(dirname(__FILE__) . '/../includes/class-*.php') as $file) {
 	require_once $file;
 }
 // Include the adapter classes test case
-require_once __DIR__ . '/../../adapter-classes-for-wordpress/tests/class-adapter_classes_testcase.php';
+require_once dirname(__FILE__) . '/../../adapter-classes-for-wordpress/tests/class-adapter_classes_testcase.php';
 ?>
